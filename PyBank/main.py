3cb06d294c7ce181345f7f4a9df4450a86a37a6d
keenet1 <keenet1@aol.com>
2023-07-19 16:03:33 -0500

    Add PyBank and PyPoll folders and contents

---
+++ PyBank/main.py
@@ -0,0 +1,73 @@
+#import os mudule and module for reading CSV files
+import os
+import csv
+csvpath = os.path.join('Starter_Code-2', 'PyBank', 'Resources', 'budget_data.csv')
+
+
+# Print Financial Anaysis Header
+print("")
+print("Financial Analysis")
+print("------------------------------")
+
+# Count the total number of months included in the dataset
+with open(csvpath) as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=',')
+    csv_header = next(csvreader)
+
+    no_lines = len(list(csvreader))
+    print(f"Total Months: {no_lines}")
+
+ #Calculate the net total Profit/Loss amount
+ #Special thanks to: https://stackoverflow.com/questions/65151369/summing-a-column-in-a-csv-file-using-python
+with open(csvpath) as budget_data:
+    data = csv.reader(budget_data, delimiter=',')
+    total = 0
+    for row in data:
+        if not str(row[1]).startswith('P'):
+            total = total + int(row[1])
+    print(f"Total: ${total}")
+
+#Calculate the average of the Profit/Loss changes
+# Special tanks to: https://github.com/fnusandhiya/python-challenge/blob/master/Pybank/main.py
+with open(csvpath) as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=',')
+    csv_header = next(csvreader)
+
+    months = []
+    profits_losses = []
+
+
+    for rows in csvreader:
+        months.append(rows[0])
+        profits_losses.append(int(rows[1]))
+
+    revenue_change = []
+
+    for x in range(1, len(profits_losses)):
+        revenue_change.append((int(profits_losses[x]) - int(profits_losses[x-1])))
+
+    revenue_average = sum(revenue_change) / len(revenue_change)
+    rounded_revenue_average = round(revenue_average,2)
+    print(f"Average Change: ${rounded_revenue_average}")
+
+#Show the greatest increase in profits (date and amount)
+    greatest_increase = max(revenue_change)
+    print(f"Greatest Increase in Profits: {months[revenue_change.index(max(revenue_change))+1]} (${greatest_increase})")
+
+#Show the greatest decrease in profits (dare and amount)
+    greatest_decrease = min(revenue_change)
+    print(f"Greatest Decrease in Profits: {months[revenue_change.index(min(revenue_change))+1]} (${greatest_decrease})")
+    print("")
+
+#Export a text file with the results
+file = open("output_PyBank.txt", "w")
+file.write("" + "\n")
+file.write("Financial Analysis" + "\n")
+file.write("------------------------------" + "\n")
+file.write(f"Total Months: {no_lines}" + "\n")
+file.write(f"Total: ${total}" + "\n")
+file.write(f"Average Change: ${rounded_revenue_average}" + "\n")
+file.write(f"Greatest Increase in Profits: {months[revenue_change.index(max(revenue_change))+1]} (${greatest_increase})" + "\n")
+file.write(f"Greatest Decrease in Profits: {months[revenue_change.index(min(revenue_change))+1]} (${greatest_decrease})" + "\n")
+file.write("" + "\n")
+file.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,86 @@
+#import os mudule and module for reading CSV files
+import os
+import csv
+csvpath = os.path.join('Starter_Code-2', 'PyPoll', 'Resources', 'election_data.csv')
+
+#create lists and/or dictionaries for candidates, percentage of votes won, & total vote counts
+#declare variables to store
+# initialize a total vote counter
+total_vote_count = 0
+
+#candidate names, vote percentage, and total vote counts
+candidate_options = []
+candidate_percentage = 0
+candidate_votes = {}
+
+#find and display the winning candidate
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+#print election results header
+print("")
+print("Election Results")
+print("------------------------------")
+file = open("output_PyPoll", "w")
+file.write("" + "\n")
+file.write("Election Results" + "\n")
+file.write("------------------------------" + "\n")
+
+#calculate and report the total votes
+#special thanks to: https://github.com/antongit505/Election_Analysis/tree/main
+with open (csvpath) as election_data:
+    csvreader = csv.reader(election_data, delimiter=',')
+    csv_header = next(csvreader)
+
+    no_lines = len(list(csvreader))
+    print(f"Total Votes: {no_lines}")
+    print("------------------------------")
+    file.write(f"Total Votes: {no_lines}" + "\n")
+    file.write("------------------------------" + "\n")
+
+
+with open(csvpath) as election_data:
+    csvreader = csv.reader(election_data, delimiter=',')
+    csv_header = next(csvreader)
+       
+    #create a for loop to count the votes for each candidate
+    for row in csvreader:
+        #add to the total vote count
+        total_vote_count = total_vote_count + 1
+
+        #get the candidate(s) name from each row
+        candidate_name = row[2]
+
+        #Get list of all unique candidate names(candidate options)
+        if candidate_name not in candidate_options:
+            #then, add the new name to the list
+            candidate_options.append(candidate_name)
+            #and also start counting their votes
+            candidate_votes[candidate_name] = 0
+
+        #add a vote to the candidates vote count total
+        candidate_votes[candidate_name] +=1
+
+    #show candidates, vote percentage, and total vote count
+    for candidate_name in candidate_votes:
+        votes = candidate_votes.get(candidate_name)
+        vote_percentage = float(votes) / float(total_vote_count) * 100
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_candidate = candidate_name
+            winning_percentage = vote_percentage
+    
+        print(f"{candidate_name}: {vote_percentage:.3F}% ({votes})")
+        file.write(f"{candidate_name}: {vote_percentage:.3F}% ({votes})" + "\n")
+
+    print("------------------------------")
+    file.write("------------------------------" + "\n")
+    print((f"Winner: {winning_candidate}"))
+    file.write(f"Winner: {winning_candidate}" + "\n")
+    print("------------------------------")
+    file.write("------------------------------" + "\n")
+    print("")
+    file.write("")
+    file.close()
